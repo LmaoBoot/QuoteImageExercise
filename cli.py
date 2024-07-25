abc02dcacb1adf5417b7ac3caedfb8a069cd8e65
LmaoBoot <81543440+LmaoBoot@users.noreply.github.com>
2024-07-25 13:23:03 -0700

    Update cli.py

--- cli.py
+++ cli.py
@@ -1,16 +1,27 @@
 import requests
 import click
 
+DEFAULT_QUOTE = 'Could not retrieve quote.'
+DEFAULT_IMAGE_TEXT = "Could not retrieve image."
+
 def get_data(key=''):
-    quote_url = f'https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en'
-    if key:
-        quote_url += f'&key={key}'
-    quote_response = requests.get(quote_url)
-    data = quote_response.json()
-    quote = data.get('quoteText', 'No quote available')
-    quote_author = data.get('quoteAuthor', 'Unknown')
-    quote_string = f'"{quote}" - {quote_author}'
-    image_url = requests.get('https://picsum.photos/300/300').url
+    try:
+        quote_url = f'https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en'
+        if key:
+            quote_url += f'&key={key}'
+        quote_response = requests.get(quote_url)
+        data = quote_response.json()
+        quote = data.get('quoteText', 'No quote available')
+        quote_author = data.get('quoteAuthor', 'Unknown')
+        quote_string = f'"{quote}" - {quote_author}'
+    except (requests.exceptions.RequestException, ValueError, KeyError):
+        quote_string = DEFAULT_QUOTE
+
+    try:
+        image_url = requests.get('https://picsum.photos/300/300').url
+    except requests.exceptions.RequestException:
+        image_url = None
+
     return {"quote": quote_string, "image_url": image_url}
 
 @click.command()
@@ -25,7 +36,10 @@ def cli(key):
 
     data = get_data(key)
     click.echo(f'Quote: {data["quote"]}')
-    click.echo(f'Image URL: {data["image_url"]}')
+    if data["image_url"]:
+        click.echo(f'Image URL: {data["image_url"]}')
+    else:
+        click.echo(click.style(DEFAULT_IMAGE_TEXT, fg='red'))
 
 if __name__ == '__main__':
     cli()
